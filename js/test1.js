406176e5e57ef20ead3e30d59daec4af1a1ff5a2
Shuangshuang Chen <shuangshuang.chen@autodesk.com>
2019-09-23 12:00:59 +0800

    js_learn

---
+++ js/test1.js
@@ -0,0 +1,38 @@
+// var a = 123;
+// var a = 'ABC';
+// alert(a);
+// // alert('我要学习JS');
+
+// if (2 > 1) {
+//     x = 1;
+//     y = 2;
+//     z = 3;
+//     if (x < y) {
+//         z = 4;
+//     }
+// }
+// new Array(1,2,3);
+
+// var arr = [1, 2, 3.14, 'Hello', null, true];
+// arr[0];
+
+// var person = {
+//     name: 'Bob',
+//     age: 20,
+// };
+// person.name;
+
+// var a;//此时a为undefined
+// var $b = 1;
+// var a_007 = '007';
+// var Answer = true;//Answer是一个布尔值true
+// var t = null;
+
+
+
+'use strict';
+// var abc = 'Hello, world';
+abc = 'Hello, world';
+console.log(abc);
+//ReferenceError: abc is not defined
+
